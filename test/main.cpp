8da2f062b4e832e4eed132cf7d4747bcf55a1b40
Sigitas Dagilis <sigis@seluxit.com>
2023-11-08 21:19:08 +0100

    chore: tmp

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,39 @@
 #include "eventdispatcherlibuv.h"
 #include "mainwindow.h"
+#include <iostream>
+#include <memory>
+#include <uvw.hpp>
 
 #include <QApplication>
 
+void listen(uvw::loop &loop) {
+    std::shared_ptr<uvw::tcp_handle> tcp = loop.resource<uvw::tcp_handle>();
+
+    tcp->on<uvw::listen_event>([](const uvw::listen_event &, uvw::tcp_handle &srv) {
+        std::shared_ptr<uvw::tcp_handle> client = srv.parent().resource<uvw::tcp_handle>();
+
+        client->on<uvw::close_event>(
+            [ptr = srv.shared_from_this()](const uvw::close_event &, uvw::tcp_handle &) { ptr->close(); });
+        client->on<uvw::end_event>([](const uvw::end_event &, uvw::tcp_handle &client) { client.close(); });
+
+        srv.accept(*client);
+        client->read();
+    });
+
+    std::cout << "Started tyo listen on local port: " << 4242 << "\n";
+
+    tcp->bind("127.0.0.1", 4242);
+    tcp->listen();
+}
+
 int main(int argc, char **argv) {
 
-    QApplication::setEventDispatcher(new qtjs::EventDispatcherLibUv);
+    auto ev_dispatcher = new qtjs::EventDispatcherLibUv();
+
+    QApplication::setEventDispatcher(ev_dispatcher);
+
     QApplication app(argc, argv);
+    ev_dispatcher->setFinalise();
 
     QApplication::setAttribute(Qt::AA_DontUseNativeMenuBar);
     MainWindow mainWindow;
